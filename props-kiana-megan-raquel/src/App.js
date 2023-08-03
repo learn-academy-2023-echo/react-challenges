418204d2c8f6fdf64c3163755e8ba3f7a6294856
raquel king <kingraq1007@gmail.com>
2023-08-03 07:58:23 -0500

    changes added to app

--- props-kiana-megan-raquel/src/App.js
+++ props-kiana-megan-raquel/src/App.js
@@ -4,24 +4,32 @@ import "./App.css"
 
 const App = () => {
   
-    const roll = [1,2,3,4,5,6]
+    const rolls = [1,2,3,4,5,6]
   
-    const [currentRoll, setCurrentRoll] = useState(roll[0])
-  
+    const [currentRoll, setCurrentRoll] = useState()
+    const [logCount, setLogCount] = useState([])
+    // state , method.                       empty array
     const triggerRoll = () => {  //function that triggers next episode
-        let nextRoll = roll.indexOf(currentRoll)
-        setCurrentRoll(roll[nextRoll])
-        console.log(nextRoll)
+      //creating random index
+       let rollIndex = Math.floor(Math.random() * rolls.length)
+       //getting value at index  .length got us the entire length of array
+        setCurrentRoll(rolls[rollIndex])
+     const rollLog = logCount.concat(currentRoll)
+     setLogCount(rollLog)
       }
     return (
     <>
       <h1>Dice Roll</h1>
-      <Box title={currentRoll} triggerNextRoll={triggerNextRoll}/> 
+      <Box title={currentRoll} triggerNextRoll={triggerRoll}/> 
       <div className="box">
-        
+        {roll.map((roll, index)=> {
+          return < roll title = {roll} key = {index}/>
+        })}
       </div>
     </>
   )
 }
 
 export default App
+
+//Storing values in array ; put in state
