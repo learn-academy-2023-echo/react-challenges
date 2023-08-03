b2155c8a23dd6db255c9904fee7ff4083c4f051b
Kiana-Massey <kxm7046@mavs.uta.edu>
2023-08-03 15:04:34 -0700

    push 2

--- functional-props-mike-bao-kiana/src/App.js
+++ functional-props-mike-bao-kiana/src/App.js
@@ -1,6 +1,9 @@
 import React from "react"
 import { useState } from "react"
 import Taco from "./components/Taco"
+import "./App.css"
+
+
 const App =() => {
   const [foodItem, setFoodItem] = useState([
     { name: "Barbacoa Tacos", price:"$5", toCart: false},
@@ -13,16 +16,24 @@ const App =() => {
   const addTaco = (indexOfSelectedTaco) => {
     foodItem[indexOfSelectedTaco].toCart = true
     setFoodItem([...foodItem])
-    // console.log(foodItem[indexOfSelectedTaco])
+    //console.log(foodItem[indexOfSelectedTaco])
   }
 
 
   return (
     <>
     <h1>Taco Slayers</h1>
+
      <div className="tacoMenu">
       {foodItem.map((foodObj, index) => {
-        return <Taco foodObj={foodObj} index={index}/>
+        return <Taco foodObj={foodObj} index={index} addTaco={addTaco}/>
+      })}
+     </div>
+
+     <div className="order">
+      <h2>Order</h2>
+      {foodItem.filter((foodObj)=> foodObj.toCart === true).map((item)=> {
+        return <h2>{item.name} Price {item.price}</h2>
       })}
      </div>
     </>
